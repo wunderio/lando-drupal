d77d323950d853a4d3ffa2a8477cef67669c17ed
Hannes Kirsman <hannes.kirsman@wunder.io>
2023-11-02 16:49:21 +0200

    #20 Better log messaged, update comments in load_extensions.sh.

--- scripts/load_extensions.sh
+++ scripts/load_extensions.sh
@@ -18,36 +18,35 @@ install_yq() {
   # Define the desired version of yq
   YQ_VERSION="4.13.3"
 
-  # Check if the `yq` binary already exists
+  # Check if the `yq` binary already exists.
   if [ -f ~/bin/yq ]; then
-    log_message "yq is already installed."
     return
   fi
 
-  # Define the architecture-specific URL
+  # Define the architecture-specific URL.
   YQ_URL="https://github.com/mikefarah/yq/releases/download/v${YQ_VERSION}/yq_linux_amd64"
 
-  # Check if the ~/bin directory exists, and if not, create it
+  # Check if the ~/bin directory exists, and if not, create it.
   [ -d ~/bin ] || mkdir -p ~/bin
 
   # Download yq binary
   wget -q "$YQ_URL" -O ~/bin/yq
   chmod +x ~/bin/yq
 
-  # Add ~/bin to PATH if not already there
+  # Add ~/bin to PATH if not already there.
   if [[ ":$PATH:" != *":$HOME/bin:"* ]]; then
     echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
     # Add it to the current shell session as well
     export PATH="$HOME/bin:$PATH"
   fi
 
-  # Inform the user that installation is complete
+  # Inform the user that installation is complete.
   log_message "yq has been installed."
 }
 
 # Make sure yq is available and assign it to yq wrapper we can use in this script.
 # If we are inside Lando, we use the freshly installed ~/bin/yq, otherwise in host
-# we use the docker container
+# we use the docker container.
 if [ -n "$LANDO" ]; then
   install_yq
 
@@ -55,7 +54,7 @@ if [ -n "$LANDO" ]; then
     ~/bin/yq "$@"
   }
 else
-  # Function to run yq using the docker container
+  # Function to run yq using the docker container.
   yq() {
     docker run --rm -v "$(pwd)":/workdir mikefarah/yq "$@"
   }
@@ -66,7 +65,7 @@ extensions=($(yq eval '.wunderio.extensions[]' .lando.yml))
 
 # Iterate over the array of extensions and merge them to project.
 for extension in "${extensions[@]}"; do
-  log_message "processing extension: $extension"
+  log_message "Installing extension: $extension"
   # Build the path to the extension-specific .lando.yml file
   extension_dir="vendor/wunderio/lando-drupal/extensions/$extension"
   extension_path="$extension_dir/.lando.yml"
@@ -76,7 +75,7 @@ for extension in "${extensions[@]}"; do
     # Use yq to merge the extension-specific .lando.yml into .lando.base.yml
     yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' .lando.base.yml "$extension_path" > .lando.base.tmp.yml
     mv .lando.base.tmp.yml .lando.base.yml
-    log_message "$extension_dir extension installed."
+    log_message "Successfully installed $extension extension."
   else
     echo "$extension_dir extension does not exist."
   fi
